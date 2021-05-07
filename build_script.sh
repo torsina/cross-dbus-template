200148c95ad7235ab7598ab8d6b8277a53071a1e
Aelis Sagot(Torsina) <aelis.sagot@gmail.com>
2021-05-07 23:12:00 +0200

    edit name of binary in target

--- build_script.sh
+++ build_script.sh
@@ -11,7 +11,7 @@ readonly LINK_FLAGS='-L /usr/aarch64-linux-gnu/lib/ -L /usr/lib/aarch64-linux-gn
 
 RUSTFLAGS=${LINK_FLAGS} cross build +nightly --release --target=${TARGET_ARCH}
 
-cp target/aarch64-unknown-linux-gnu/release/report-creator $HOME/test
+cp target/aarch64-unknown-linux-gnu/release/cross-dbus-template $HOME/test
 
 chmod +x $HOME/test
 
